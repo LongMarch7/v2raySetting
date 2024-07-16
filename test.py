2b0aafcc591172513b4e8cde6b364c5e305e21d6
1024T <18046057402@189.cn>
2024-07-16 11:57:13 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,14 @@
-#!/usr/bin/env python
-#-*- coding:utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import BaseHTTPServer
+import http.server
 import hashlib
 import os
-import urllib2
+import urllib.request
 import sys
 import re
-reload(sys)
-sys.setdefaultencoding('utf-8')
 
-class CacheHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class CacheHandler(http.server.BaseHTTPRequestHandler):
     Page = '''\
         <html>
         <body>
@@ -33,10 +31,10 @@ class CacheHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             '''
     def do_GET(self):
         if self.path == "/url":
-            page = self.create_page(r'v2ray url')
+            page = self.create_page('v2ray url')
             self.send_page(page)
         elif self.path == "/ssinfo":
-            page = self.create_page(r'v2ray ssinfo')
+            page = self.create_page('v2ray ssinfo')
             self.send_page(page)
 
     def send_page(self, page):
@@ -44,7 +42,7 @@ class CacheHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html; charset=UTF-8")
         self.send_header("Content-Length", str(len(page)))
         self.end_headers()
-        self.wfile.write(page)
+        self.wfile.write(page.encode('utf-8'))
 
     def create_page(self, cmd):
         page = self.start + self.get_url(cmd) + self.end
@@ -52,13 +50,13 @@ class CacheHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     def get_url(self, cmd):
         with os.popen(cmd, 'r') as f:
-            text = f.read().decode('utf8')
-            url=re.sub(r'\033\[\d{1,2}m', "", text)
+            text = f.read()
+            url = re.sub(r'\033\[\d{1,2}m', "", text)
             return url
 
 def run():
     server_address = ('', 80)
-    httpd = BaseHTTPServer.HTTPServer(server_address, CacheHandler)
+    httpd = http.server.HTTPServer(server_address, CacheHandler)
     httpd.serve_forever()
 
 if __name__ == '__main__':
