dce950b7dcb97c36d298b77eb0d363914debf332
1024T <18046057402@189.cn>
2024-12-20 09:12:23 +0800

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -2,6 +2,6 @@
 # chkconfig: 345 90 90
 # description: autostart......
 cd /root/v2ray_client
-nohup ./v2ray -config=config.json &
+nohup ./v2ray run -config=config.json &
 cd /root
 nohup python /root/test.py &
